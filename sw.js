d405e631943a4ba30dedcf927ff981a6a0a46fc2
kmuwanga83 <kmuwanga@bugemauniv.ac.ug>
2026-07-22 15:51:30 +0300

    Add service worker and .nojekyll for PWA support

---
+++ sw.js
@@ -0,0 +1,50 @@
+// Service Worker for Bugema Notice Board
+const CACHE_NAME = 'buk-notice-board-v1';
+const urlsToCache = [
+    '/bugema-notice-board/',
+    '/bugema-notice-board/index.html',
+    '/bugema-notice-board/style.css',
+    '/bugema-notice-board/script.js',
+    '/bugema-notice-board/bugema_logo.png'
+];
+
+// Install event - cache essential files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event - serve from cache first, then network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
